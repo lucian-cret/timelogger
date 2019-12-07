0c72a7fac97fa4ab96ced89bfdd158b50d4feed0
Lucian Cret <lucicret@gmail.com>
2019-12-07 22:34:24 +0200

    fix date sorting, improved validation for worked hours

--- TimeLogger/TimeLogger/Models/TimeLogs/LogTimeViewModel.cs
+++ TimeLogger/TimeLogger/Models/TimeLogs/LogTimeViewModel.cs
@@ -6,7 +6,8 @@ namespace TimeLogger.Models
     {
         public int ProjectId { get; set; }
         [Display(Name = "Worked hours")]
-        [Range(0.5, 8.0)]
+        [RegularExpression("^[0-9]\\.?[0-9]{0,2}$", ErrorMessage = "The field \"Worked hours\" must have only 2 decimals")]
+        [Range(0.5, 8, ErrorMessage = "The field \"Worked hours\" must be between 0,5 and 8.")]
         public float WorkedHours { get; set; }
         [Required]
         public string Description { get; set; }
