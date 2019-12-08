b531fa9d82739ce2b2d12bbb7746295c0ed452bd
Lucian Cret <lucicret@gmail.com>
2019-12-08 21:11:01 +0200

    unit tests for filters

--- TimeLogger/TimeLogger.UnitTests/Filters/FiltersCommonsTests.cs
+++ TimeLogger/TimeLogger.UnitTests/Filters/FiltersCommonsTests.cs
@@ -15,12 +15,12 @@ namespace TimeLogger.UnitTests.Filters
     public class FiltersCommonsTests : BaseTests
     {
         private FiltersCommon _filtersCommon;
+        private ActionExecutingContext _context;
 
-        [Fact]
-        public void GetProjectIdFromRequest_ProjectIdAndTimeLogIdNotFound_ReturnsNull()
+        public FiltersCommonsTests()
         {
             var httpContext = new DefaultHttpContext();
-            var context = new ActionExecutingContext(
+            _context = new ActionExecutingContext(
                 new ActionContext
                 {
                     HttpContext = httpContext,
@@ -30,10 +30,14 @@ namespace TimeLogger.UnitTests.Filters
                 new List<IFilterMetadata>(),
                 new Dictionary<string, object>(),
                 new Mock<Controller>().Object);
+        }
 
+        [Fact]
+        public void GetProjectIdFromRequest_ProjectIdAndTimeLogIdNotFound_ReturnsZero()
+        {
             _filtersCommon = new FiltersCommon();
 
-            var result = _filtersCommon.GetProjectIdFromRequest(context, Mock.Of<TimeLoggerDbContext>());
+            var result = _filtersCommon.GetProjectIdFromRequest(_context, Mock.Of<TimeLoggerDbContext>());
 
             Assert.Equal(0, result);
         }
@@ -41,22 +45,10 @@ namespace TimeLogger.UnitTests.Filters
         [Fact]
         public void GetProjectIdFromRequest_ProjectIdFound_ReturnsValue()
         {
-            var httpContext = new DefaultHttpContext();
-            var context = new ActionExecutingContext(
-                new ActionContext
-                {
-                    HttpContext = httpContext,
-                    RouteData = new RouteData(),
-                    ActionDescriptor = new ActionDescriptor()
-                },
-                new List<IFilterMetadata>(),
-                new Dictionary<string, object>(),
-                new Mock<Controller>().Object);
-
-            context.ModelState.SetModelValue("projectId", "1", "1");
+            _context.ModelState.SetModelValue("projectId", "1", "1");
             _filtersCommon = new FiltersCommon();
 
-            var result = _filtersCommon.GetProjectIdFromRequest(context, Mock.Of<TimeLoggerDbContext>());
+            var result = _filtersCommon.GetProjectIdFromRequest(_context, Mock.Of<TimeLoggerDbContext>());
 
             Assert.Equal(1, result);
         }
@@ -64,19 +56,7 @@ namespace TimeLogger.UnitTests.Filters
         [Fact]
         public void GetProjectIdFromRequest_ProjectIdNotFoundAndTimeLogIdFound_ReturnsProjectIdFromTimeLogDbEntryValue()
         {
-            var httpContext = new DefaultHttpContext();
-            var context = new ActionExecutingContext(
-                new ActionContext
-                {
-                    HttpContext = httpContext,
-                    RouteData = new RouteData(),
-                    ActionDescriptor = new ActionDescriptor()
-                },
-                new List<IFilterMetadata>(),
-                new Dictionary<string, object>(),
-                new Mock<Controller>().Object);
-
-            context.ModelState.SetModelValue("timeLogId", "1", "1");
+            _context.ModelState.SetModelValue("timeLogId", "1", "1");
             using (DbContextInMemory = GetDatabaseContext(nameof(GetProjectIdFromRequest_ProjectIdNotFoundAndTimeLogIdFound_ReturnsProjectIdFromTimeLogDbEntryValue)))
             {
                 DbContextInMemory.TimeLogs.Add(new TimeLog { Id = 1, ProjectId = 5 });
@@ -86,9 +66,24 @@ namespace TimeLogger.UnitTests.Filters
 
             using (DbContextInMemory = GetDatabaseContext(nameof(GetProjectIdFromRequest_ProjectIdNotFoundAndTimeLogIdFound_ReturnsProjectIdFromTimeLogDbEntryValue)))
             {
-                var result = _filtersCommon.GetProjectIdFromRequest(context, DbContextInMemory);
+                var result = _filtersCommon.GetProjectIdFromRequest(_context, DbContextInMemory);
 
                 Assert.Equal(5, result);
+            }
+        }
+
+        [Fact]
+        public void GetProjectIdFromRequest_ProjectIdNotFoundAndTimeLogIdFoundButNoLogEntryInDb_ReturnsZero()
+        {
+            _context.ModelState.SetModelValue("timeLogId", "1", "1");
+            DbContextInMemory = GetDatabaseContext(nameof(GetProjectIdFromRequest_ProjectIdNotFoundAndTimeLogIdFoundButNoLogEntryInDb_ReturnsZero));
+            _filtersCommon = new FiltersCommon();
+
+            using (DbContextInMemory = GetDatabaseContext(nameof(GetProjectIdFromRequest_ProjectIdNotFoundAndTimeLogIdFoundButNoLogEntryInDb_ReturnsZero)))
+            {
+                var result = _filtersCommon.GetProjectIdFromRequest(_context, DbContextInMemory);
+
+                Assert.Equal(0, result);
             }
         }
     }
---
+++ TimeLogger/TimeLogger.UnitTests/Filters/RedirectToListIfNotAllowedTests.cs
@@ -0,0 +1,109 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Abstractions;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using Moq;
+using System;
+using System.Collections.Generic;
+using TimeLogger.DAL;
+using TimeLogger.DAL.Entities;
+using TimeLogger.Filters;
+using Xunit;
+
+namespace TimeLogger.UnitTests.Filters
+{
+    public class RedirectToListIfNotAllowedTests : BaseTests
+    {
+        private RedirectToListIfNotAllowed _filter;
+
+        private readonly Mock<ILogger<RedirectToListIfNotAllowed>> _mockLogger = new Mock<ILogger<RedirectToListIfNotAllowed>>();
+        private readonly Mock<TimeLoggerDbContext> _mockDbContext = new Mock<TimeLoggerDbContext>();
+        private readonly Mock<IFiltersCommon> _mockFiltersCommon = new Mock<IFiltersCommon>();
+
+        private ActionExecutingContext _context;
+
+        public RedirectToListIfNotAllowedTests()
+        {
+            _filter = new RedirectToListIfNotAllowed(_mockDbContext.Object, _mockLogger.Object, _mockFiltersCommon.Object);
+            var httpContext = new DefaultHttpContext();
+            _context = new ActionExecutingContext(
+                new ActionContext
+                {
+                    HttpContext = httpContext,
+                    RouteData = new RouteData(),
+                    ActionDescriptor = new ActionDescriptor()
+                },
+                new List<IFilterMetadata>(),
+                new Dictionary<string, object>(),
+                new Mock<Controller>().Object);
+        }
+
+        [Fact]
+        public void OnActionExecuting_ProjectIdNotFound_ContextResultIsRedirectToProjectList()
+        {
+            _mockFiltersCommon.Setup(s => s.GetProjectIdFromRequest(It.IsAny<ActionExecutingContext>(), It.IsAny<TimeLoggerDbContext>())).Returns(0);
+
+            _filter.OnActionExecuting(_context);
+
+            var contextResult = Assert.IsType<RedirectToActionResult>(_context.Result);
+            Assert.Equal("ProjectsList", contextResult.ActionName);
+            Assert.Equal("Projects", contextResult.ControllerName);
+        }
+
+        [Fact]
+        public void OnActionExecuting_ProjectIdFoundInRequestNotDb_ContextResultIsRedirectToProjectList()
+        {
+            _mockFiltersCommon.Setup(s => s.GetProjectIdFromRequest(It.IsAny<ActionExecutingContext>(), It.IsAny<TimeLoggerDbContext>())).Returns(1);
+            _mockDbContext.Setup(s => s.Projects).Returns(Mock.Of<DbSet<Project>>());
+
+            _filter.OnActionExecuting(_context);
+
+            var contextResult = Assert.IsType<RedirectToActionResult>(_context.Result);
+            Assert.Equal("ProjectsList", contextResult.ActionName);
+            Assert.Equal("Projects", contextResult.ControllerName);
+        }
+
+        [Fact]
+        public void OnActionExecuting_ProjectIdFoundInRequestAndDbNotCompleted_ContextResultIsNull()
+        {
+            _mockFiltersCommon.Setup(s => s.GetProjectIdFromRequest(It.IsAny<ActionExecutingContext>(), It.IsAny<TimeLoggerDbContext>())).Returns(1);
+            using (DbContextInMemory = GetDatabaseContext(nameof(OnActionExecuting_ProjectIdFoundInRequestAndDbNotCompleted_ContextResultIsNull)))
+            {
+                DbContextInMemory.Projects.Add(new Project { Id = 1, Deadline = DateTime.Today.AddDays(10) });
+                DbContextInMemory.SaveChanges();
+            }
+
+            using (DbContextInMemory = GetDatabaseContext(nameof(OnActionExecuting_ProjectIdFoundInRequestAndDbNotCompleted_ContextResultIsNull)))
+            {
+                _filter = new RedirectToListIfNotAllowed(DbContextInMemory, _mockLogger.Object, _mockFiltersCommon.Object);
+                _filter.OnActionExecuting(_context);
+
+                Assert.Null(_context.Result);
+            }
+        }
+
+        [Fact]
+        public void OnActionExecuting_ProjectIdFoundInRequestAndDbCompleted_ContextResultRedirectToTimesLogList()
+        {
+            _mockFiltersCommon.Setup(s => s.GetProjectIdFromRequest(It.IsAny<ActionExecutingContext>(), It.IsAny<TimeLoggerDbContext>())).Returns(1);
+            using (DbContextInMemory = GetDatabaseContext(nameof(OnActionExecuting_ProjectIdFoundInRequestAndDbCompleted_ContextResultRedirectToTimesLogList)))
+            {
+                DbContextInMemory.Projects.Add(new Project { Id = 1, Deadline = DateTime.Today.AddDays(-5) });
+                DbContextInMemory.SaveChanges();
+            }
+
+            using (DbContextInMemory = GetDatabaseContext(nameof(OnActionExecuting_ProjectIdFoundInRequestAndDbCompleted_ContextResultRedirectToTimesLogList)))
+            {
+                _filter = new RedirectToListIfNotAllowed(DbContextInMemory, _mockLogger.Object, _mockFiltersCommon.Object);
+                _filter.OnActionExecuting(_context);
+
+                var contextResult = Assert.IsType<RedirectToActionResult>(_context.Result);
+                Assert.Equal("TimeLogsList", contextResult.ActionName);
+                Assert.Equal("TimeLogs", contextResult.ControllerName);
+            }
+        }
+    }
+}
