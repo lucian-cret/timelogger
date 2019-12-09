1be9fbf6df4ccf0467ec818493ac528ac9b49eb3
Lucian Cret <lucicret@gmail.com>
2019-12-09 22:14:55 +0200

    added time logs integration tests

---
+++ TimeLogger/TimeLogger.IntegrationTests/AntiForgeryTokenExtractor.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TimeLogger.IntegrationTests
+{
+    public static class AntiForgeryTokenExtractor
+    {
+        public static string AntiForgeryFieldName { get; } = "AntiForgeryTokenField";
+        public static string AntiForgeryCookieName { get; } = "AntiForgeryTokenCookie";
+    }
+}
---
+++ TimeLogger/TimeLogger.IntegrationTests/TimeLogsControllerTests.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace TimeLogger.IntegrationTests
+{
+    public class TimeLogsControllerTests : IClassFixture<WebApplicationFactory<Startup>>
+    {
+        private readonly HttpClient _client;
+
+        public TimeLogsControllerTests(WebApplicationFactory<Startup> factory)
+        {
+            _client = factory.CreateClient();
+        }
+
+        [Fact]
+        public async Task TimeLogsList_ProjectId1_ReturnsViewWithTimeLogsListForProject1()
+        {
+            var response = await _client.GetAsync("/TimeLogs/TimeLogsList/?projectId=1");
+
+            response.EnsureSuccessStatusCode();
+
+            var responseString = await response.Content.ReadAsStringAsync();
+
+            Assert.Contains("Write unit tests", responseString);
+            Assert.Contains("Change framework version", responseString);
+        }
+
+        [Fact]
+        public async Task TimeLogsList_NoProjectIdInQueryString_ReturnsViewWithProjectsList()
+        {
+            var response = await _client.GetAsync("/TimeLogs/TimeLogsList");
+
+            response.EnsureSuccessStatusCode();
+
+            var responseString = await response.Content.ReadAsStringAsync();
+
+            Assert.Contains("Test Project 1", responseString);
+            Assert.Contains("Test Project 1 Description", responseString);
+        }
+
+        [Fact]
+        public async Task LogTimeGet_ProjectId1_ReturnsViewWithTimeLogsListForProject1()
+        {
+            var response = await _client.GetAsync("/TimeLogs/LogTime/?projectId=1");
+
+            response.EnsureSuccessStatusCode();
+
+            var responseString = await response.Content.ReadAsStringAsync();
+
+            Assert.Contains("Worked hours", responseString);
+            Assert.Contains("Description", responseString);
+        }
+
+        [Fact]
+        public async Task LogTimeGet_NoProjectIdInQueryString_ReturnsViewWithProjectsList()
+        {
+            var response = await _client.GetAsync("/TimeLogs/LogTime");
+
+            response.EnsureSuccessStatusCode();
+
+            var responseString = await response.Content.ReadAsStringAsync();
+
+            Assert.Contains("Test Project 1", responseString);
+            Assert.Contains("Test Project 1 Description", responseString);
+        }
+    }
+}
